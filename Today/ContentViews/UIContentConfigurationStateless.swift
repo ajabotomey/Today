45513b3a00cdf5dc8788c58a6282e8e630a2ee3e
ajabotomey <killerbyte_93@hotmail.com>
2024-01-28 15:06:42 +1000

    forgot to stage all files

--- Today/ContentViews/UIContentConfigurationStateless.swift
+++ Today/ContentViews/UIContentConfigurationStateless.swift
@@ -5,4 +5,10 @@
 //  Created by Andrew Abotomey on 28/1/2024.
 //
 
-import Foundation
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
